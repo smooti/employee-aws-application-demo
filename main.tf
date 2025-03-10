54673c76edb81b780c3863e79b70d4b9003d9980
smooti <kobeowens43099@gmail.com>
2025-03-10 19:55:02 -0400

    adding IAM role resources

--- main.tf
+++ main.tf
@@ -1,3 +1,37 @@
+# Create IAM resource
+resource "aws_iam_role" "web-app" {
+  name = "EmployeeWebAppRole"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect : "Allow",
+        Principal : {
+          Service : "ec2.amazonaws.com"
+        },
+        Action : "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "EmployeeWebAppRole"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "amazon-dynamodb-full-access" {
+  role       = aws_iam_role.web-app.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess" # Built-in policy
+}
+
+resource "aws_iam_role_policy_attachment" "amazon-s3-full-access" {
+  role       = aws_iam_role.web-app.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess" # Built-in policy
+}
+
 # Create a VPC with public and private subnets spanning multiple availability zones
 module "vpc" {
   source          = "./modules/aws-vpc"
@@ -23,8 +57,8 @@ module "sgs" {
 
 # Create S3 bucket for user image storage
 resource "aws_s3_bucket" "photos" {
-  bucket = "employee-photo-bucket-rko-493"
-	force_destroy = true
+  bucket        = "employee-photo-bucket-rko-493"
+  force_destroy = true
 
   tags = {
     Name = "employee-photo-bucket-rko-493"
@@ -60,7 +94,7 @@ resource "aws_instance" "employee-web-app" {
 
   ami                         = "ami-05b10e08d247fb927" # Amazon Linux
   instance_type               = var.ec2_instance_type
-  iam_instance_profile        = "EmployeeWebAppRole" # Has Amazons S3 and Amazon DynamoDB FullAccess
+  iam_instance_profile        = aws_iam_role.web-app.name # Has Amazons S3 and Amazon DynamoDB FullAccess
   vpc_security_group_ids      = [module.sgs.security_group_id]
   subnet_id                   = module.vpc.public_subnets_id[0]
   associate_public_ip_address = true

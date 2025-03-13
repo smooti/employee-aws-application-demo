91177621bf015514b8ee4859504743f95235283b
smooti <kobeowens43099@gmail.com>
2025-03-12 21:41:46 -0400

    Adding load balancer dns output

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@
 #   description = "Public IP address of ec2 instance"
 #   value       = aws_instance.employee-web-app.public_ip
 # }
+
+output "load-balancer-dns-name" {
+	description = "Load balancer DNS name"
+	value = aws_lb.app.dns_name
+}
